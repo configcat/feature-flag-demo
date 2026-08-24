400152da54c588ed08156094ec18303c9f04ed62
Lajos Szoke <lali@configcat.com>
2026-08-24 15:33:02 +0200

    dockerize service

---
+++ start_cf_pages.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -eu
+
+DIST_BASE_DIR=./dist/feature-flag-demo
+
+for JSFILE in $DIST_BASE_DIR/*.js
+do 
+    echo replacing variable in $JSFILE
+    VARIABLES=$(grep -o "$JSFILE" -e '\$\{CC_[a-zA-Z_0-9]*\}' -E | tr '\n' ' ')
+    envsubst "$VARIABLES" < "$JSFILE" > "$DIST_BASE_DIR/tempjsfile.js"
+    mv "$DIST_BASE_DIR/tempjsfile.js" "$JSFILE"
+done
